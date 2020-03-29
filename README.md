ff9e05dca43a771ea89fa5143204e82b342713d3
angelpsch <angelpsch@users.noreply.github.com>
2020-03-29 09:18:43 -0500

    -A

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Password Generator 
+### Use to create random, secure password.
 [Link to deployed application](https://angelpsch.github.io/password-generator/)
+
+### How to use
+- When "Generate Password" button is pressed, you will be asked: 
+    - If you want lowercase letters in your password.
+    - If you want uppercase letters in your password. 
+    - If you want numbers in your password. 
+    - If you want special (ASCII) characters in your password.
+    - Set a number between 8 and 128 characters for password length. 
+- Once all questions are complete your password will be generated based on the criteria inputted.
--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 
-// get desired password len and check to make sure it is between 8 & 128 char
+// Get desired password len and check to make sure it is between 8 & 128 char
 function getlen() {
   do {
     var length = prompt("How long do you want your password to be? It has to be at least 8 characters, but no longer than 128 characters.");
@@ -16,34 +16,61 @@ function getlen() {
   while (len < 8 || len > 128 || isNaN(len) == true || len == '');
 }
 
+// To verify that no confirms are 
+function checkConfirm(array) {
+  if (array.includes(true)) {
+    return true;
+} else {
+  return false; 
+}
+}
 
-
-function generatePassword(pass) {
-  var lowerConfirm = confirm("Do you want lowercase in your password?");
-  var upperConfirm = confirm("Do you want uppercase in your password?");
-  var numConfirm = confirm("Do you want numbers in your password?");
-  var specialConfirm = confirm("Do you want special characters in your password?");
+// Ask user what character types they would like to include 
+function getCharacters(charset) {  
   var passGen = {
     lowercase: "abcdefghijklmnopqrstuvwxyz",
     uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     numbers: "0123456789",
     special: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
   };
-  var len = getlen();
-  console.log(len);
+  do {
+  var lowerConfirm = confirm("Do you want lowercase in your password?");
+  var upperConfirm = confirm("Do you want uppercase in your password?");
+  var numConfirm = confirm("Do you want numbers in your password?");
+  var specialConfirm = confirm("Do you want special characters in your password?");
   var charset = '';
+  var ckConfirm = []; 
   if (lowerConfirm == true) {
     charset += passGen.lowercase;
+    ckConfirm.push(true); 
   }
   if (upperConfirm == true) {
     charset += passGen.uppercase;
+    ckConfirm.push(true); 
   }
   if (numConfirm == true) {
     charset += passGen.numbers;
+    ckConfirm.push(true); 
   }
   if (specialConfirm == true) {
     charset += passGen.special;
+    ckConfirm.push(true); 
   }
+  var checkComp = checkConfirm(ckConfirm);
+  if (checkComp == false) {
+    alert ("Error: cannot create password with no character types.");
+  } else {
+    return charset; 
+  }
+  
+} while (checkComp == false); 
+}
+
+// Generate password based on user input
+function generatePassword(pass) {
+  var charset = getCharacters(); 
+  var len = getlen();
+  console.log(len);
   console.log(charset);
 
   var pass = '';
@@ -54,6 +81,7 @@ function generatePassword(pass) {
   console.log(pass);
   return pass;
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
